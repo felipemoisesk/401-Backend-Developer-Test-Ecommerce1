0cdacc683ffaf838978e9354c0e78e08504a5f20
Felipe Möises <69239349+felipemoisesk@users.noreply.github.com>
2022-12-27 01:07:33 -0300

    Script Update Resolution

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
+def get_four_digit_numbers(max_digit):
+  result = []
+  for a in range(max_digit+1):
+    for b in range(max_digit+1):
+      for c in range(max_digit+1):
+        for d in range(max_digit+1):
+          if a + b + c + d == 21:
+            result.append(str(a) + str(b) + str(c) + str(d))
+  return result
+
 maxDigit = input('Digite um número de 1 a 9: ')
-
-d1 = f'{maxDigit}'
-d2 = f'{maxDigit}'
-d3 = f'{maxDigit}'
-d4 = f'{maxDigit}'
-
-numax = int(d1 + d2 + d3 + d4)
-
-#print(type(numax))
-print(numax)
-#print(list(str(numax)))
-listanumeros = []
-numeros = []
-for numero in range(1000, numax+1, 1):
-    algarismos = list(str(numero))
-    somanumero = int(algarismos[0]) + int(algarismos[1]) + int(algarismos[2]) + int(algarismos[3])
-    if somanumero == 21:
-        if int(algarismos[0]) <= int(maxDigit) and int(algarismos[1]) <= int(maxDigit) and int(algarismos[2]) <= int(maxDigit) and int(algarismos[3]) <= int(maxDigit):
-#            listanumeros.append(algarismos)
-            numeros.append(numero)
-        else:
-            pass
-#print (listanumeros)
-print (numeros)
-print (len(numeros))
+result = get_four_digit_numbers(maxDigit)
+print(result)
